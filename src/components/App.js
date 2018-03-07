185af51a6afb95ff7df59089689db764693b3027
Andrew Patipaksiri <apax714@gmail.com>
2018-03-07 12:24:54 -0800

    init project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar0light bg-dark">
+        <a href="#" className="narbar-brand">
+          Navbar
+        </a>
+      </nav>
+    </div>
+  );
+};
