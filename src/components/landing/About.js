f9d735dc9f6992e7e77a3ad08107b8fd877cc0bb
Andrew Patipaksiri <apax714@gmail.com>
2018-03-07 17:29:20 -0800

    add

---
+++ src/components/landing/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <section id="about-section" className="container">
+        <div className="div border-bottom">
+          <h1 className="text-left display-4 font-italic">worktitle</h1>
+        </div>
+      </section>
+    </div>
+  );
+};
