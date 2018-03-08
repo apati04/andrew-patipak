499fd64057bf5de1c30ade5cdb1fb38f5345df2e
Andrew Patipaksiri <apax714@gmail.com>
2018-03-07 16:45:38 -0800

    test

---
+++ src/components/landing/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <section id="main" className="bg-primary">
+      <h1 className="text-center">My portfolio</h1>
+      <p className="lead">Learn more about what i do</p>
+      <button className="btn btn-default">contact me!</button>
+    </section>
+  );
+};
